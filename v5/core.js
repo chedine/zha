e7ad74cc076f129d09d5cc1606805bce7ee4ac39
dinesh <babusd@gmail.com>
2018-06-20 23:43:45 -0500

    improvements to V5

--- v5/core.js
+++ v5/core.js
@@ -51,7 +51,7 @@ const ZHATYPE = function () {
         ZhaFn: function (fn) {
             this.value = fn;
             this.invoke = function (env, args) {
-                var returnVal = this.value.apply(undefined, args);
+                var returnVal = this.value.call(this, env, args);
                 return returnVal;
             };
             this.toString = function () {
@@ -76,7 +76,7 @@ const ZHATYPE = function () {
 			this.third = function(val){
 				return this.value[2];
 			};
-			this.nth = function(val, n){
+			this.nth = function(n){
 				return this.value[n];
 			};
 			this.concat=function(zhaList){
@@ -86,7 +86,7 @@ const ZHATYPE = function () {
 				return this.value;
 			};
 			this.rest=function(){
-				return new ZhaList(this.value.slice(1));
+				return new ZHATYPE.ZhaList(this.value.slice(1));
 			}
         },
 
@@ -146,22 +146,13 @@ const ZHATYPE = function () {
  */
 const RT = function () {
     return {
-        add: makeForeignFn("return a + b;", "a", "b"),
-        sub: makeForeignFn("return a - b;", "a", "b"),
-        div: makeForeignFn("return a / b;", "a", "b"),
-        mul: makeForeignFn("return a * b;", "a", "b"),
-        iff: makeForeignFn1((pred, truthyPath) => {
-            console.log(truthyPath);
-            if (pred) {
-                return ZHATYPE.isFn(truthyPath) ? truthyPath() : truthyPath;
-            }
-            return false;
-        }, ["pred", "truthyPath"]),
-        ">": makeForeignFn1((left, right) => left > right),
-        "<": makeForeignFn1((left, right) => left < right),
-        "+": makeForeignFn1((left, right) => left + right),
-        "-": makeForeignFn1((left, right) => left - right),
-        "=": makeForeignFn1((left, right) => left === right)
+        ">": makeForeignFn1((env, args) => new ZHATYPE.ZhaBoolean(args.first().value < args.second().value)),
+        "<": makeForeignFn1((env, args) => new ZHATYPE.ZhaBoolean(args.first().value > args.second().value)),
+        "+": makeForeignFn1((env, args) => new ZHATYPE.ZhaNumber(args.first().value + args.second().value)),
+        "-": makeForeignFn1((env, args) => new ZHATYPE.ZhaNumber(args.first().value - args.second().value)),
+        "/": makeForeignFn1((env, args) => new ZHATYPE.ZhaNumber(args.first().value / args.second().value)),
+        "*": makeForeignFn1((env, args) => new ZHATYPE.ZhaNumber(args.first().value * args.second().value)),
+        "eq?": makeForeignFn1((env, args) => new ZHATYPE.ZhaBoolean(args.first().value === args.second().value))
     }
 }();
 const ENVIRONMENT = function (runtime, root	) {
@@ -175,12 +166,12 @@ const ENVIRONMENT = function (runtime, root	) {
 	this.get = function(symbol){
 		const rt = this.lookup(symbol);
 		if(rt !== null && rt !== undefined){
-			return rt.bindings[symbol];
+			return rt.bindings[symbol.value];
 		}
 		throw new Error("Symbol "+symbol.value +" cannot be located.");
 	}
     this.lookup = function (symbol) {
-        if (this.bindings.hasOwnProperty(key)) {
+        if (this.bindings.hasOwnProperty(symbol.value)) {
             return this;
         } else if (root !== undefined) {
             return root.lookup(symbol);
@@ -191,7 +182,7 @@ const ENVIRONMENT = function (runtime, root	) {
     }
     this.define = function (sym, val) {
         //TODO: check if sym is a symbol
-        this.bindings[sym] = val;
+        this.bindings[sym.value] = val;
     }
 };
 
@@ -201,13 +192,13 @@ const Eval = function(){
 		//TODO:Read and eval
 	};
 	this.evalExp = function(form, env){
-		
+		return this.evalExpWithEnv(form, env);
 	};
 	this.evalExpWithEnv = function(form, env){
 		if(ZHATYPE.isList(form)){
-			evalListWithEnv(form, env);
+			return this.evalListWithEnv(form, env);
 		}
-		else if(ZHATYPE.isList(form)){
+		else if(ZHATYPE.isSymbol(form)){
 			const bounded = env.get(form);
 			//TODO: Why need this check to return form as-is
 			return bounded === null ? form: bounded;
@@ -217,7 +208,7 @@ const Eval = function(){
 		}
 	};
 	this.evalListWithEnv = function (ast, env){
-		let firstSymbol = ast.first();
+		let first = ast.first();
 		if(ZHATYPE.isSymbol(first)){
 			const keyWord = first.value;
 			if(keyWord === "def"){
@@ -253,7 +244,7 @@ const Eval = function(){
 				const e = this;
 				return new ZHATYPE.ZhaFn((env, values) => {
 					const closure = new ENVIRONMENT({}, env);
-					for(var i=0;i<fnArgs.length;i++){
+					for(var i=0;i<fnArgs.value.length;i++){
 						closure.define(fnArgs.nth(i), values.nth(i));
 					}
 					return e.evalExpWithEnv(fnBody, closure);
@@ -262,20 +253,20 @@ const Eval = function(){
 			else{
 				const dispatcher = env.get(first);
 				if(ZHATYPE.isFn(dispatcher)){
-					return dispatcher.invoke(env, this.expandExprs(ast.rest(),env));
+					return dispatcher.invoke(env, this.expandAttrs(ast.rest(),env));
 				}else if (dispatcher != null){
 					return dispatcher;
 				}
 			}
 		}
 		else if(ZHATYPE.isList(first)){
-			let fn_res = evalListWithEnv(first, env);
+			let fn_res = this.evalListWithEnv(first, env);
 			let flat_exp = new ZHATYPE.ZhaList();
 			flat_exp.conj(fn_res);
 			for(v in ast.rest().getList()){
 				flat_exp.conj(v);
 			}
-			return evalListWithEnv(flat_exp, env);			
+			return this.evalListWithEnv(flat_exp, env);			
 		}
 		else{
 			const fn = first;
@@ -286,8 +277,132 @@ const Eval = function(){
 	this.expandAttrs = function(listForm, env){
 		var evaled = [];
 		for(var i=0;i<listForm.getList().length;i++){
-			evaled.push(evalExp(listForm.nth(i), env));
+			evaled.push(this.evalExp(listForm.nth(i), env));
 		}
 		return new ZHATYPE.ZhaList(evaled);
 	}
 }
+
+const Reader = function(){
+    const READ_TABLE = {
+        "`": this.readQuasiQuote
+    };
+    
+    this.read = function (program) {
+        const ast = readExpr(program, 0, new typeSystem.ZhaList([]));
+        return ast;
+    };
+    this.readMacro = function (token, program, currentPos, astSoFar){
+        READ_TABLE[token](program,currentPos, astSoFar);
+    };
+    
+    readQuasiQuote = function(program, currentPos, astSoFar){
+        var form = (readExpr(program, currentPos + 1, new ZhaList([])));
+        var listForm = form.first();
+        var intercepted = [new Symbol("list")].concat(listForm.value);
+        astSoFar.push(new ZhaList(intercepted));
+    }
+    
+    function isReaderMacro(token){
+        return READ_TABLE[token] !== undefined;
+    }
+    
+    function readExpr(program, startPos, ast) {
+        //const ast = baseAST;
+        let buf = '';
+        let readingQuotes = false;
+        i = startPos;
+    
+        const addToAST = (token) => {
+            if (token.length > 0) {
+                ast.conj(typeSystem.typeIfy(token));
+            }
+        }
+        for (i = startPos; i < program.trim().length; i++) {
+            const char = program.charAt(i);
+            if(isReaderMacro(char)){
+                readMacro(char, program, i , ast);
+            //	console.log("resuming from "+ i , ast.toString());
+                i = i-1;
+                continue;
+            }
+            if (char === '"') {
+                if (buf.length > 0) {
+                    if (readingQuotes) {
+                        buf = buf + '"';
+                        addToAST(buf);
+                        buf = '';
+                        readingQuotes = false;
+                    } else {
+                        console.log(`Error encountered quotes @ pos ${i} in ${program}`);
+                    }
+                } else {
+                    readingQuotes = true;
+                    buf = buf + '"';
+                }
+            } else if (char === ' ') {
+                if (!readingQuotes) {
+                    if (buf.trim().length > 0) {
+                        addToAST(buf);
+                        buf = '';
+                    }
+                } else {
+                    buf = buf + char;
+                }
+            } else if (char === '(') {
+                ast.conj(readExpr(program, i + 1, new typeSystem.ZhaList([])));
+            } else if (char === ')') {
+                addToAST(buf);
+                buf = '';
+                return ast;
+            } else if (!readingQuotes &&
+                char === '\r' || char === '\n' || char === '\r\n') {
+                addToAST(buf);
+                //addToAST("\n");
+                buf = '';
+            } else if (char === '\t') {
+                addToAST(buf);
+                buf = '';
+            } else {
+                buf += char;
+            }
+        }
+        if (buf.trim().length > 0) {
+            addToAST(buf);
+        }
+        return ast;
+    }
+    
+    function isMacroCall(ast, env) {
+        if(!isList(ast) || !isSymbol(ast.first())){
+            return false;
+        }
+        var present = env.isPresent(ast.first().value);
+        return present && env.lookup(ast.first().value).isMacro;
+    }
+    
+    function macroExpand(ast, env){
+        if(!isMacroCall(ast, env)){
+            return ast;
+        }
+        var macroExpander = env.lookup(ast.first().value);
+        var expandedForm = macroExpander.invoke(ast.rest());
+        console.log("Expanded as ", expandedForm);
+        return expandedForm;
+    }
+}
+
+const reader = new Reader();
+const evaler = new Eval();
+const typeSystem = ZHATYPE;
+const rootEnv = new ENVIRONMENT(RT, undefined);
+
+const run = function(program){
+    var asts = reader.read(program);
+    for(var i=0; i<asts.getList().length;i++){
+//        console.log(asts.nth(i));
+        return evaler.evalExp(asts.nth(i), rootEnv);
+    }
+    
+}
+
